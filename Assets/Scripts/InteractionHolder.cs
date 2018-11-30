d568ff50fb731d4d479e8157db814166fe74b27d
Casimir Kuusela <crk.kuusela@gmail.com>
2018-11-30 18:24:06 +0200

    Answer2 done

---
+++ Assets/Scripts/InteractionHolder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionHolder : MonoBehaviour
+{
+
+    public float radiusModifier = 1;
+    [SerializeField]
+    GameObject[] objects;
+    Vector4[] positions = new Vector4[100];
+
+
+    void Start()
+    {
+
+        foreach (var o in objects)
+        {
+
+            Shader.SetGlobalFloat("_Radius", o.GetComponent<Renderer>().bounds.extents.magnitude * radiusModifier);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        for (int i = 0; i < objects.Length; i++)
+        {
+            positions[i] = objects[i].transform.position;
+        }
+        Shader.SetGlobalFloat("_PositionArray", objects.Length);
+        Shader.SetGlobalVectorArray("_Positions", positions);
+
+    }
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+
+    // Use this for initialization
+    public float movementSpeed = 3;
+	public float rotateSpeed = 200;
+    private Rigidbody rb;
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+    }
+}
